adf4450461345bdd27b8f2b8dfc19a75ad0bbd35
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-02-19 11:53:26 -0800

    jquery is finally rollong. One more print for debug

--- app.js
+++ app.js
@@ -137,6 +137,7 @@ function getImgUrl(access) {
     $.get('https://api.instagram.com/v1/users/self/media/recent/' + '?' + url_param,
          function(result) {
              var temp_url = result.data[0].images.standard_resolution.url;
+             console.log(temp_url);
              var img_url = temp_url.split("?")[0];
              console.log(img_url);
          });
