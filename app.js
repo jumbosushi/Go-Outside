c66aee1f677b9326d6c3413e7b25603e432efedc
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-02-19 16:29:45 -0800

    Refactor

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Express dependencies
 var http = require('http');
 var clarifai = require('./clarifai');
+var insta = require('./instagram.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var $ = require('jQuery');
@@ -130,52 +131,10 @@ ig.use({
     client_secret: "95b76f3db7314eaea2bfefd9569a33ec"
 });
 
-
-// authorize the user by redirecting user to sign in page
-exports.authorize_user = function (req, res) {
-    res.redirect(ig.get_authorization_url(redirect_uri))
-};
-
-// send message on #general that the user is signed in
-exports.handleauth = function (req, res) {
-    ig.authorize_user(req.query.code, redirect_uri, function (err, result) {
-        if (err) {
-            console.log(err.body);
-            res.send("Didn't work");
-            slack.send({
-                text: "Login Unseccessful :("
-            });
-        } else {
-            console.log('Yay! Access token is ' + result.access_token);
-            access_token = result.access_token;
-            slack.send({
-                text: "Log in Successful!\n Welcome to Go Outside homie!" +
-                      "\n Wait a moment till your Instagram is subscribed to the game"
-            });
-        }
-    });
-
-    // Instagram subscription
-    // initializes the user profile
-    ig.add_user_subscription('https://lit-journey-12058.herokuapp.com/user',
-        function (err, result, remaining, limit) {
-            users[result.id] = {
-                "access": access_token,
-                "name": user_name,
-                "score": 0
-            };
-            console.log(users);
-    });
-    res.end('Joey is proud of ya');
-};
-
-
-
-
 // api initially send users to authorize
-app.get('/authorize_user', exports.authorize_user);
+app.get('/authorize_user', insta.authorize_user);
 // redirecting URI is handled here
-app.get('/handleauth', exports.handleauth);
+app.get('/handleauth', insta.handleauth);
 
 
 // ---------------------------------
--- instagram.js
+++ instagram.js
@@ -1,1 +1,44 @@
+
+var $ = require('jQuery');
+var ig = require('instagram-node').instagram({});
+
 // All Instagram Modules should be moved her
+module.exports = {
+
+    // send message on #general that the user is signed in
+    handleauth: function (req, res) {
+                    ig.authorize_user(req.query.code, redirect_uri, function (err, result) {
+                        if (err) {
+                            console.log(err.body);
+                            res.send("Didn't work");
+                            slack.send({
+                                text: "Login Unseccessful :("
+                            });
+                        } else {
+                            console.log('Yay! Access token is ' + result.access_token);
+                            access_token = result.access_token;
+                            slack.send({
+                                text: "Log in Successful!\n Welcome to Go Outside homie!" +
+                                      "\n Wait a moment till your Instagram is subscribed to the game"
+                            });
+                        };
+                     });
+                        // Instagram subscription
+                        // initializes the user profile
+                        ig.add_user_subscription('https://lit-journey-12058.herokuapp.com/user',
+                            function (err, result, remaining, limit) {
+                                users[result.id] = {
+                                    "access": access_token,
+                                    "name": user_name,
+                                    "score": 0
+                                };
+                                console.log(users);
+                        });
+                        res.end('Joey is proud of ya');
+                    },
+
+    // authorize the user by redirecting user to sign in page
+    authorize_user = function (req, res) {
+                        res.redirect(ig.get_authorization_url(redirect_uri))
+                    }
+}
