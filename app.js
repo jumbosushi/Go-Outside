c72327c9ec00a83c4493f85a72d8ca468a8cef14
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-02-19 16:18:24 -0800

    Code refactored

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Express dependencies
 var http = require('http');
 var clarifai = require('./clarifai');
 var express = require('express');
@@ -11,17 +12,14 @@ var slack = new Slack("https://hooks.slack.com/services/T0N3CEYE5/B0N49BWJ1/XUsV
 var app = express();
 var port = process.env.PORT || 3000;
 
-// hold access Token
-var access_token;
-
-// will contain user information
-// key = user's subscription id | val = access_token
-var users = {};
+var access_token; // hold the access Token of a user
+var users = {}; // key = user's subscription id,  val = access_token
 var user_name;
 var user_id;
 
-// Joey's random words
-var shit_hikers_say = {
+
+// Joey's random talks
+var shit_joey_say = {
     // found on http://iandolij.tumblr.com/post/50120585724/shit-hikers-say
     0: "Hike your own hike, man",
     1: "What’s your base weight",
@@ -30,7 +28,7 @@ var shit_hikers_say = {
     4: "No rain no pain no Maine",
     5: "How’s the water source look?",
     6: "How many miles you doing today",
-    7: "Dang It smells like hiker in here" ,
+    7: "Dang It smells like hiker in here",
     8: "How’s the privy look",
     9: "No bear cables?",
     10: "Duck the smokeys",
@@ -61,31 +59,25 @@ var redirect_uri = "https://lit-journey-12058.herokuapp.com/handleauth"; //TODO
 // https://lit-journey-12058.herokuapp.com/handleauth
 
 // body parser middleware
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-
-// --------------------------------
-// Instagram Authentification
-// Overide instagram authentification
-ig.use({
-  client_id: "bd09eab6bd9b4c9daf691a550faf04a9",
-  client_secret: "95b76f3db7314eaea2bfefd9569a33ec"
-});
-
+// -----------------------------
 // Slash commands
 app.post('/slash', function (req, res) {
 
     var slash_text = req.body.text;
-    console.log(slash_text);
     // "login" - let the user log in
     if (slash_text == "login") {
         user_name = req.body.user_name; // store the username temporally
         user_id = req.body.user_id; // store the user_id temporally
         slack.send({
             text: "<https://lit-journey-12058.herokuapp.com/authorize_user|Join the party!>" +
-                  "\n You'll need your insagram account."//TODO
+                  "\n You'll need your insagram account."
         });
+
 
     // "stats" - show the current standing of people in the game
     } else if (slash_text == "stats") {
@@ -95,28 +87,29 @@ app.post('/slash', function (req, res) {
             });
         };
 
-    // coconut oil
+
+    // "coconut oil" - coconut oil
     } else if (slash_text == "coconut oil") {
-            slack.send({
-                text: "Now you're speaking my language"
-            });
-
-    // "help" - help tag
-    } else if (slash_text == "help") {
-            slack.send({
-                text: "Hey there! This is yo boy Joey \n" +
-                      "I will keep in track of how you're team is doing Go Outside challenge \n" +
-                      "Here's how its played. Whenever you post a picture, I'll check if you took \n" +
-                      "that picture outside. If you did, congrats! You get a point \n"
-            });
-
-    // Joey also talks with as well
-    } else if (slash_text == "talk")  {
         slack.send({
-                text: shit_hikers_say[parseInt(Math.random() * 30 )]
+            text: "Now you're speaking my language :taco: :taco: :taco:"
         });
 
-       // else return something else
+    // "help" - explain the game
+    } else if (slash_text == "help") {
+        slack.send({
+            text: "Hey there! This is yo boy Joey \n" +
+                "I will keep in track of how your team is doing in the Go Outside game :rocket: \n" +
+                "Here's how its played. Whenever you post a picture on Instagram, I'll check if you took \n" +
+                "that picture outside. If you did, congrats! You get a point \n"
+        });
+
+    // "talk" - Choose a random phrase from shit_joey_say
+    } else if (slash_text == "talk") {
+        slack.send({
+            text: shit_joey_say[parseInt(Math.random() * 30)]
+        });
+
+    // everything else
     } else {
         slack.send({
             text: "Hmm not sure what tag that is. You mean Bacon?"
@@ -126,46 +119,59 @@ app.post('/slash', function (req, res) {
 });
 
 
-// Authorize the user by redirecting user to sign in page
-exports.authorize_user = function(req, res) {
+// -------------------------------------------
+
+// Instagram Authentification
+// overide instagram authentification
+ig.use({
+    client_id: "bd09eab6bd9b4c9daf691a550faf04a9",
+    client_secret: "95b76f3db7314eaea2bfefd9569a33ec"
+});
+
+
+// authorize the user by redirecting user to sign in page
+exports.authorize_user = function (req, res) {
     res.redirect(ig.get_authorization_url(redirect_uri))
 };
 
-// Send message on #general that the user is signed in
-exports.handleauth = function(req, res) {
-  ig.authorize_user(req.query.code, redirect_uri, function(err, result) {
-    if (err) {
-      console.log(err.body);
-      res.send("Didn't work");
-      slack.send({
-            text: "Login Unseccessful :("
-      });
-    } else {
-      console.log('Yay! Access token is ' + result.access_token);
-      access_token = result.access_token;
-      slack.send({
-            text: "Log in Successful!\n Welcome to Go Outside homie!"
-      });
-    }
-  });
-  // Instagram subscription
-  // initializes the user profile
-  ig.add_user_subscription('https://lit-journey-12058.herokuapp.com/user',
-                               function(err, result, remaining, limit){
-                                    users[result.id] = {"access": access_token,
-                                                        "name": user_name,
-                                                        "score": 0};
-                                    console.log(users);
-                               });
-  res.end('It worked!');
+// send message on #general that the user is signed in
+exports.handleauth = function (req, res) {
+    ig.authorize_user(req.query.code, redirect_uri, function (err, result) {
+        if (err) {
+            console.log(err.body);
+            res.send("Didn't work");
+            slack.send({
+                text: "Login Unseccessful :("
+            });
+        } else {
+            console.log('Yay! Access token is ' + result.access_token);
+            access_token = result.access_token;
+            slack.send({
+                text: "Log in Successful!\n Welcome to Go Outside homie!"
+            });
+        }
+    });
+
+    // Instagram subscription
+    // initializes the user profile
+    ig.add_user_subscription('https://lit-journey-12058.herokuapp.com/user',
+        function (err, result, remaining, limit) {
+            users[result.id] = {
+                "access": access_token,
+                "name": user_name,
+                "score": 0
+            };
+            console.log(users);
+    });
+    res.end('Joey is proud of ya');
 };
 
 
 
 
-// This is where api initially send users to authorize
+// api initially send users to authorize
 app.get('/authorize_user', exports.authorize_user);
-// This is redirecting URI
+// redirecting URI is handled here
 app.get('/handleauth', exports.handleauth);
 
 
@@ -173,17 +179,18 @@ app.get('/handleauth', exports.handleauth);
 // Instagram subscrription API
 
 // Subscribe the user when the user loggs in
-app.get('/user', function(req, res) {
+app.get('/user', function (req, res) {
     slack.send({
-            text: "You're part of the club! \n Remember, the first rule of Go Outside Club is to always talk about Go Outside Club"
+        text: "You're part of the club! \n Remember, " +
+              "the first rule of Go Outside Club is to always talk about Go Outside Club"
     });
     // Instagram API completes subscription when 'hub.challenge' is send back
     res.send(req.query['hub.challenge']);
 });
 
-// Fired when any of the user uploads a new file.
+// Fired when any of the registered user uploads a new file.
 // Check if the file uploaded counts as a point.
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
     var sub_id = req.body[0]['subscription_id'];
     if (users[sub_id]) {
         slack.send({
@@ -194,21 +201,22 @@ app.post('/user', function(req, res) {
         check_outdoor(temp_result, users[sub_id]["name"], users[sub_id]);
     };
     res.send("New activity from the subcription detected");
-    }
-);
+});
 
 function getImgUrl(access) {
-    var url_param = $.param({access_token: access})
+    var url_param = $.param({
+        access_token: access
+    })
     $.get('https://api.instagram.com/v1/users/self/media/recent/' + '?' + url_param,
-         function(result) {
-             var temp_url = result.data[0].images.standard_resolution.url;
-             console.log(temp_url);
-             var img_url = temp_url.split("?")[0];
-             console.log(img_url);
-             var result = clarifai.run(img_url);
+        function (result) {
+            var temp_url = result.data[0].images.standard_resolution.url;
+            console.log(temp_url);
+            var img_url = temp_url.split("?")[0];
+            console.log(img_url);
+            var result = clarifai.run(img_url);
             console.log(result);
             return result;
-    });
+        });
 };
 
 // check if the picture was take outside
@@ -216,10 +224,9 @@ function getImgUrl(access) {
 function check_outdoor(result, name, user) {
     result = result.split(", ");
     for (var tag in result) {
-        if ( tag == "outside" || tag == "street") {
+        if (tag == "outside" || tag == "street") {
             slack.send({
-            text: name + " did it! Way to go for being outside! (wait wut?)"
-                  + "You get 1 points."
+                text: name + " did it! Way to go for being outside! (wait wut?)" + "You get 1 points."
             });
             user["score"] += 1;
             return;
@@ -230,7 +237,9 @@ function check_outdoor(result, name, user) {
 // -------------------------------------
 
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello World!') });
+app.get('/', function (req, res) {
+    res.status(200).send('Hello World!')
+});
 
 // error handler
 app.use(function (err, req, res, next) {
