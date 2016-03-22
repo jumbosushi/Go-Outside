bb3e37612817fbb9553d99b85618ff036775ffd8
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-03-21 22:30:43 -0700

    Fix type of jquery in clarifai.js

--- clarifai.js
+++ clarifai.js
@@ -10,7 +10,7 @@ module.exports = function (imgurl) {
   		return;
   	}
 
-  	$ = require("jquery")(window);
+  	$ = require("jQuery")(window);
   });
 
   var LocalStorage = require('node-localstorage').LocalStorage,
