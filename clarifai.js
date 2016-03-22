b408d5097859df4568f973958204eb9a6c8907a8
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-03-21 21:38:37 -0700

    Move all helper functions inside modular.exports in clarifai.js

--- clarifai.js
+++ clarifai.js
@@ -1,73 +1,71 @@
 // Cloned from https://github.com/cassidoo/clarifai-javascript-starter
-var $ = require('jQuery');
+module.exports = function (imgurl) {
+  console.log("INSIDE clarifai.js!");
+  console.log(imgurl);
 
-var Use_Clarifai = function () {};
+  var $ = require('jQuery');
 
-var LocalStorage = require('node-localstorage').LocalStorage,
-localStorage = new LocalStorage('./scratch');
+  var LocalStorage = require('node-localstorage').LocalStorage,
+  localStorage = new LocalStorage('./scratch');
 
-function getCredentials(cb) {
-  var data = {
-    'grant_type': 'client_credentials',
-    'client_id': "LPOXuuwXLHA2yZ7fBrN_DAHTsu26s2mR9h4DVmMa",
-    'client_secret': "VMEOyjHqQqIRRdpNL-o8wmfEpnsObF9ksIaPJ2Yt"
-  };
+  function getCredentials(cb) {
+    var data = {
+      'grant_type': 'client_credentials',
+      'client_id': "LPOXuuwXLHA2yZ7fBrN_DAHTsu26s2mR9h4DVmMa",
+      'client_secret': "VMEOyjHqQqIRRdpNL-o8wmfEpnsObF9ksIaPJ2Yt"
+    };
 
-  return $.ajax({
-    'url': 'https://api.clarifai.com/v1/token',
-    'data': data,
-    'type': 'POST'
-  })
-  .then(function(r) {
-    localStorage.setItem('accessToken', r.access_token);
-    localStorage.setItem('tokenTimestamp', Math.floor(Date.now() / 1000));
-    cb();
-  });
-}
-
-function postImage(imgurl) {
-  var data = {
-    'url': imgurl
-  };
-  var accessToken = localStorage.getItem('accessToken');
-
-  return $.ajax({
-    'url': 'https://api.clarifai.com/v1/tag',
-    'headers': {
-      'Authorization': 'Bearer ' + accessToken
-    },
-    'data': data,
-    'type': 'POST'
-  }).then(function(r){
-    parseResponse(r);
-  });
-}
-
-function parseResponse(resp) {
-  var tags = [];
-  if (resp.status_code === 'OK') {
-    var results = resp.results;
-    tags = results[0].result.tag.classes;
-    console.log("clarifai.js working ok!");
-  } else {
-    console.log('Sorry, something is wrong.');
+    return $.ajax({
+      'url': 'https://api.clarifai.com/v1/token',
+      'data': data,
+      'type': 'POST'
+    })
+    .then(function(r) {
+      localStorage.setItem('accessToken', r.access_token);
+      localStorage.setItem('tokenTimestamp', Math.floor(Date.now() / 1000));
+      cb();
+    });
   }
 
-  $('#tags').text(tags.toString().replace(/,/g, ', '));
-  console.log("clarifai.js tags is about to be returned!");
-  return tags;
+  function postImage(imgurl) {
+    var data = {
+      'url': imgurl
+    };
+    var accessToken = localStorage.getItem('accessToken');
+
+    return $.ajax({
+      'url': 'https://api.clarifai.com/v1/tag',
+      'headers': {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      'data': data,
+      'type': 'POST'
+    }).then(function(r){
+      parseResponse(r);
+    });
+  }
+
+  function parseResponse(resp) {
+    var tags = [];
+    if (resp.status_code === 'OK') {
+      var results = resp.results;
+      tags = results[0].result.tag.classes;
+      console.log("clarifai.js working ok!");
+    } else {
+      console.log('Sorry, something is wrong.');
+    }
+
+    $('#tags').text(tags.toString().replace(/,/g, ', '));
+    console.log("clarifai.js tags is about to be returned!");
+    return tags;
+  }
+
+  if (localStorage.getItem('tokenTimeStamp') - Math.floor(Date.now() / 1000) > 86400
+  || localStorage.getItem('accessToken') === null) {
+  getCredentials(function() {
+    return postImage(imgurl);
+  });
+  } else {
+    return postImage(imgurl);
+  }
 }
-
-
-module.exports = function (imgurl) {
-                    console.log("INSIDE clarifai.js!");
-                    console.log(imgurl);
-                    if (localStorage.getItem('tokenTimeStamp') - Math.floor(Date.now() / 1000) > 86400
-                      || localStorage.getItem('accessToken') === null) {
-                      getCredentials(function() {
-                        return postImage(imgurl);
-                      });
-                    } else {
-                      return postImage(imgurl);
-                    }
-                  }
